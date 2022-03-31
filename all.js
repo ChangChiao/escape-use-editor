6ca48e73f74394279c908bbc92927c415a8f7ae3
joe <joe.chang1014@gmail.com>
2022-03-31 10:26:11 +0800

    feat imageUploader

--- all.js
+++ all.js
@@ -20,6 +20,8 @@ function imageHandler() {
   }
 }
 
+Quill.register("modules/imageUploader", ImageUploader);
+
 const quill = new Quill("#editor", {
   theme: "snow",
   modules: {
@@ -29,8 +31,19 @@ const quill = new Quill("#editor", {
     toolbar: {
       container: myToolbar,
       handlers: {
-        image: imageHandler,
+        // image: imageHandler,
       },
     },
+    imageUploader: {
+        upload: file => {
+            return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(
+                "https://images.unsplash.com/photo-1486427944299-d1955d23e34d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+                );
+            }, 1000);
+            });
+        }
+    }
   },
 });
